65bb03cd61dd1265648c19487d0f2fc339aa8ce9
Alex Xie <xieay336@gmail.com>
2023-09-07 19:51:51 -0500

    fixed typo

--- September/September.java
+++ September/September.java
@@ -13,7 +13,7 @@ public class September {
         byte[] bytes = asciiString.getBytes();
         StringBuilder binary = new StringBuilder();
         for (byte b : bytes) {
-            int val = b;fasdf
+            int val = b;
             for (int i = 0; i < 8; i++) {
                 binary.append((val & 128) == 0 ? 0 : 1);
                 val <<= 1;
